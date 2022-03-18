90ed750017ff3cce3ababca523412f57c2e22d34
IT21164330 <99310968+IT21164330@users.noreply.github.com>
2022-03-18 10:14:28 +0000

    did

--- Student.h
+++ Student.h
@@ -1,9 +1,11 @@
 class Student {
   // private section
-  //    int studentId
-  //    name <- 20 charcters
-  
-  // public section
-  //    assignDetails() method declaration
+  priavte:
+    int studentID;
+    char name[20];
+  public :
+  //    assignDetails() method declaration\
+    void assignDetails(int id,char n[20]);
   //    display() method declaration
+    void display();
 };
